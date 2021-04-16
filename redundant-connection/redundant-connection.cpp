f7bfeee29a33767057cb8f5ccc5837a939579d34
Devansh Suwalka <devanshsuwalka@gmail.com>
2021-04-16 21:53:44 +0530

    Time: 4 ms (97.99%), Space: 8.9 MB (62.83%) - LeetHub

---
+++ redundant-connection/redundant-connection.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=684 lang=cpp
+ *
+ * [684] Redundant Connection
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> id;
+    int find(int c)
+    {
+        int root = id[c];
+        while (root != c)
+        {
+            c = root;
+            root = id[root];
+        }
+        return root;
+    }
+    vector<int> findRedundantConnection(vector<vector<int>> &e)
+    {
+        int n = e.size();
+        id.resize(n);
+        vector<int> ans;
+        iota(id.begin(), id.end(), 0);
+        for (int i = 0; i < n; i++)
+        {
+            int x = find(e[i][0] - 1);
+            int y = find(e[i][1] - 1);
+            if (x != y)
+            {
+                id[y] = x;
+            }
+            else
+            {
+                ans.emplace_back(e[i][0]);
+                ans.emplace_back(e[i][1]);
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
