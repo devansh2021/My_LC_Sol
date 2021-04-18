886069c875e9c5531f0acdfaddee6b3c9b259ed6
Devansh Suwalka <devanshsuwalka@gmail.com>
2021-04-18 21:54:33 +0530

    Update find-xor-sum-of-all-pairs-bitwise-and.cpp

--- find-xor-sum-of-all-pairs-bitwise-and/find-xor-sum-of-all-pairs-bitwise-and.cpp
+++ find-xor-sum-of-all-pairs-bitwise-and/find-xor-sum-of-all-pairs-bitwise-and.cpp
@@ -3,9 +3,12 @@ class Solution
 public:
     int getXORSum(vector<int> &a, vector<int> &b)
     {
-        int res = 0, xa = accumulate(begin(a), end(a), 0, [](int s, int i) { return s ^ i; });
-        for (auto n : b)
-            res = res ^ (xa & n);
-        return res;
+        int ans = 0;
+        int xora = 0;
+        for (auto x : a)
+            xora ^= x;
+        for (auto x : b)
+            ans = ans ^ (xora & x);
+        return ans;
     }
 };
