261d6f04a07ce673eec74acfb23f108057be4c3c
Devansh Suwalka <devanshsuwalka@gmail.com>
2021-06-02 12:30:00 +0530

    Time: 28 ms (5.97%), Space: 16.4 MB (5.09%) - LeetHub

--- jump-game-ii/jump-game-ii.cpp
+++ jump-game-ii/jump-game-ii.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int jump(vector<int>& v) {
-        vector<int> dp(v.size(), INT_MAX);
-        dp[0]=0;
-        for(int i=0;i<v.size();i++)
+     int far=0, i=0, curfar=0, ans=0, n=v.size();
+        for(;i<n&&i<=far;i++)
         {
-            if(dp[i]!=INT_MAX)
+            if(i>curfar)
             {
-                for(int j=1;j<=v[i]&&i+j<v.size();j++)
-                {
-                    dp[i+j]=min(dp[i]+1, dp[i+j]);
-                }
+                curfar=far;
+                ans++;
             }
+            far=max(far, i+v[i]);
         }
-        return dp[v.size()-1];
+        return ans;
     }
 };
