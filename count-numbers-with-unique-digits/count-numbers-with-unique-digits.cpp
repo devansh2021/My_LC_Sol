ba407c82037e72100a87979af2434df90f8bc08a
Devansh Suwalka <devanshsuwalka@gmail.com>
2021-06-20 15:32:14 +0530

    Time: 0 ms (100.00%), Space: 6 MB (34.89%) - LeetHub

---
+++ count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        if(n==0)
+            return 1;
+        vector<int> dp(n+1);
+        dp[0]=1;
+        dp[1]=10;
+        for(int i=2;i<=n;i++)
+         dp[i]=(dp[i-1]-dp[i-2])*(10-i+1)+dp[i-1];
+        return dp[n];
+    }
+};
